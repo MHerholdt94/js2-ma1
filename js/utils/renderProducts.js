941d2517fc914fdced489f22e47138db5e31c590
MHerholdt94 <mathiasher@live.no>
2023-03-24 15:32:28 +0100

    2nd commit [24.03.23]

--- js/utils/renderProducts.js
+++ js/utils/renderProducts.js
@@ -7,12 +7,56 @@ export function renderProducts(product) {
   productsContainer.innerHTML = "";
 
   product.forEach(function (product) {
+    let cssClass = "add-btn";
+
+    const itemIsAdded = wishlist.find(function (item) {
+      return parseInt(item.id) === product.id;
+    });
+
+    if (itemIsAdded) {
+      cssClass = "added-btn";
+    }
+
     productsContainer.innerHTML += `<div class="product">
                                         <h4>${product.title}</h4>
                                         <div>
-                                            <p>£${product.price}</p>
-                                            <button>Wishlist</button>
+                                          <button class="default-btn ${cssClass}" data-id="${product.id}" data-title="${product.title}" data-price="${product.price}">Wishlist</button>
+                                          <p>£${product.price}</p>
                                         </div>
                                     </div>`;
   });
+
+  const addButtons = document.querySelectorAll(".default-btn");
+
+  addButtons.forEach((btn) => {
+    btn.addEventListener("click", addClick);
+  });
+
+  function addClick() {
+    this.classList.toggle("add-btn");
+    this.classList.toggle("added-btn");
+
+    const id = this.dataset.id;
+    const title = this.dataset.title;
+    const price = this.dataset.price;
+
+    const currentWishlist = getWishlist();
+
+    const productExists = currentWishlist.find(function (item) {
+      return item.id === id;
+    });
+
+    if (productExists === undefined) {
+      const product = { id: id, title: title, price: price };
+      currentWishlist.push(product);
+      saveWishlist(currentWishlist);
+    } else {
+      const newWishlist = currentWishlist.filter((item) => item.id !== id);
+      saveWishlist(newWishlist);
+    }
+  }
+
+  function saveWishlist(items) {
+    localStorage.setItem("wishlist", JSON.stringify(items));
+  }
 }
--- js/wishlist.js
+++ js/wishlist.js
@@ -1,19 +1,63 @@
 import { getWishlist } from "./utils/getWishlist.js";
+import { displayMessage } from "./constants/messages.js";
 
 const wishlist = getWishlist();
 
-const productsContainer = document.queryselector(".products");
+const productsContainer = document.querySelector(".products");
+
+productsContainer.innerHTML = "";
 
 if (wishlist.length === 0) {
-  productsContainer.innerHTML = "You have nothing added to your wishlist yet";
+  displayMessage(
+    "empty-wishlist",
+    "<p>:(</p>You have nothing added to your wishlist yet",
+    ".products"
+  );
 }
 
 wishlist.forEach((product) => {
   productsContainer.innerHTML += `<div class="product">
                                         <h4>${product.title}</h4>
                                         <div>
-                                            <p>${product.price}</p>
-                                            <button>Remove</button>
+                                          <button class="remove-btn" data-id="${product.id}" data-title="${product.title}" data-price="${product.price}">Remove</button>
+                                          <p>£${product.price}</p>
                                         </div>
                                     </div>`;
 });
+
+const removeButtons = document.querySelectorAll(".remove-btn");
+
+removeButtons.forEach((btn) => {
+  btn.addEventListener("click", removeClick);
+});
+
+function removeClick() {
+  this.parentElement.parentElement.style.display = "none";
+
+  const id = this.dataset.id;
+  const currentWishlist = getWishlist();
+  const productExists = currentWishlist.find(function (item) {
+    return item.id === id;
+  });
+
+  if (productExists === undefined) {
+    const product = { id: id };
+    currentWishlist.pop(product);
+    saveWishlist(currentWishlist);
+  } else {
+    const newWishlist = currentWishlist.filter((item) => item.id !== id);
+    saveWishlist(newWishlist);
+  }
+}
+
+function saveWishlist(items) {
+  localStorage.setItem("wishlist", JSON.stringify(items));
+
+  if (items.length === 0) {
+    displayMessage(
+      "empty-wishlist",
+      "<p>:(</p>You have nothing added to your wishlist yet",
+      ".products"
+    );
+  }
+}
